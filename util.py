4f003c0e4ebfe0b4d79ecaff7db0c3cef53b6338
Mike Frampton <mikeframpo@gmail.com>
2014-08-24 20:12:46 +1200

    Added a prime sieve function.

--- util.py
+++ util.py
@@ -21,3 +21,10 @@ def findprimes(n):
             primes.append(val)
     return primes
 
+def prime_sieve(n):
+    isprime = [True] * (n-1)
+    for p in range(2, n+1):
+        for pmult in range(2, n/p + 1):
+            isprime[(p * pmult) - 2] = False
+    return [i+2 for i in range(len(isprime)) if isprime[i]]
+
